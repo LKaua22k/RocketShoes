89475f1309c528485af82128b9c05f71f03d8f05
Lkaua22k <lkauan462@gmail.com>
2023-03-16 00:28:26 -0300

    :tada:

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ let menu = document.querySelector('.menu')
 
 menu.addEventListener('click',() => {
     teste.classList.toggle('on',show);
+    
+    document.querySelector('.logo').style.display = show ? 'none' : 'block';
+    document.body.style.overflow = show ? 'hidden' : 'initial';
 
     show = !show;
 })
+
+const pes = document.querySelector('.search');
+
+function testeClick(){
+    pes.onclick = () => {
+        if(pes.classList.toggle('active')){
+            menu.style.display = 'none';  
+            document.querySelector('.logo').style.display = 'none';
+        }else {
+            menu.style.display = 'initial';   
+            document.querySelector('.logo').style.display = 'initial';  
+        }
+          
+         
+    }
+}
