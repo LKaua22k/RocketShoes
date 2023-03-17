85f0a0f2574ebd7f10b46ed2b3396930ac123e8e
Lkaua22k <lkauan462@gmail.com>
2023-03-17 00:40:50 -0300

    :tada:

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let menu = document.querySelector('.menu')
 
 menu.addEventListener('click',() => {
     teste.classList.toggle('on',show);
+
     
-    document.querySelector('.logo').style.display = show ? 'none' : 'block';
     document.body.style.overflow = show ? 'hidden' : 'initial';
 
     show = !show;
@@ -18,10 +18,10 @@ const pes = document.querySelector('.search');
 function testeClick(){
     icon.onclick = () => {
         if(pes.classList.toggle('active')){
-            menu.style.display = 'none';  
+            document.querySelector('.logo').style.display = 'none';
         }else {
             menu.style.display = 'initial';   
-                document.querySelector('.logo').style.display = 'initial';  
+            document.querySelector('.logo').style.display = 'block';  
         }
           
          
