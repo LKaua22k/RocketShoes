5b23d00fa8db34ef1487eae79e6630d5b353e64a
Lkaua22k <lkauan462@gmail.com>
2023-03-24 00:12:46 -0300

    :tada:

--- script.js
+++ script.js
@@ -1,55 +1,30 @@
 let show = true;
 
-let teste = document.querySelector('.teste');
-let menu = document.querySelector('.menu')
+let teste = document.querySelector(".teste1");
+let menu = document.querySelector(".menu");
 
-function menuClick(){
-    menu.onclick = () => {
-        teste.classList.toggle('on',show);
+function menuClick() {
+  menu.onclick = () => {
+    teste.classList.toggle("on", show);
 
-        document.body.overflow = show ? 'hidden' : 'initial';
-    
-        show = !show;
-    }
+    document.body.overflow = show ? "hidden" : "initial";
+
+    show = !show;
+  };
 }
 
-const icon = document.querySelector('.icon');
-const pes = document.querySelector('.search');
+const mails = document.getElementsByClassName("mail");
+const activeImg = document.getElementsByClassName("hover");
 
-document.body.onresize = function testeClick(){
-    if(document.body.clientWidth < 480){
-        icon.onclick = () => {
-            if(pes.classList.toggle('active')){
-                document.querySelector('.logo').style.display = 'none';
-            }else {
-                menu.style.display = 'initial';   
-                document.querySelector('.logo').style.display = 'block';  
-            } 
-        }
+for (var i = 0; i < mails.length; i++) {
+  mails[i].addEventListener("click", function () {
+    console.log(activeImg);
+
+    if (activeImg.length >= 0) {
+      activeImg[0].classList.remove("hover");
     }
 
-
-}
-
-const mails = document.getElementsByClassName('mail')
-const activeImg = document.getElementsByClassName('hover')
-
-
-for (var i = 0; i < mails.length ; i++){
-
-    mails[i].addEventListener('click', function(){
-        console.log(activeImg)
-
-        if (activeImg.length >= 0) {
-            activeImg[0].classList.remove('hover')
-        }
-
-        this.classList.add('hover');
-        document.getElementById('featured').src = this.src
-    })
-    
-}
-
-// if (activeImg.length >= 0) {
-//     activeImg[0].classList.remove('hover')
-// }
+    this.classList.add("hover");
+    document.getElementById("featured").src = this.src;
+  });
+  };
